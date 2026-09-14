800e50ed6b00eed6d155995758f1a3487099bc16
Csongusz <74738349+Csongusz@users.noreply.github.com>
2026-09-14 11:28:57 +0200

    Max létszám ellenőrzés, már tagja vagy, nincs ilyen szakkör/felhasználó bugfix

--- src/main/java/hu/csongor/demo/controller/JelentkezesController.java
+++ src/main/java/hu/csongor/demo/controller/JelentkezesController.java
@@ -1,7 +1,10 @@
 package hu.csongor.demo.controller;
 
 import hu.csongor.demo.entity.Jelentkezes;
+import hu.csongor.demo.entity.Szakkor;
 import hu.csongor.demo.repository.JelentkezesRepository;
+import hu.csongor.demo.repository.SzakkorRepository;
+import hu.csongor.demo.repository.UserRepository;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.time.LocalDateTime;
@@ -11,17 +14,33 @@ import java.time.LocalDateTime;
 public class JelentkezesController {
 
     private final JelentkezesRepository jelentkezesRepository;
+    private final UserRepository userRepository;
+    private final SzakkorRepository szakkorRepository;
 
     public JelentkezesController(
-            JelentkezesRepository jelentkezesRepository) {
-
+            JelentkezesRepository jelentkezesRepository,
+            UserRepository userRepository,
+            SzakkorRepository szakkorRepository) {
         this.jelentkezesRepository = jelentkezesRepository;
+        this.userRepository = userRepository;
+        this.szakkorRepository = szakkorRepository;
     }
 
     @PostMapping
     public Object jelentkezes(
             @RequestBody Jelentkezes jelentkezes){
 
+        if(!userRepository.existsById(
+                jelentkezes.getUserId())) {
+
+            return "Nincs ilyen felhasználó!";
+        }
+
+        if(!szakkorRepository.existsById(
+                jelentkezes.getSzakkorId())) {
+
+            return "Nincs ilyen szakkör!";
+        }
         if(jelentkezesRepository
                 .existsByUserIdAndSzakkorId(
                         jelentkezes.getUserId(),
@@ -29,7 +48,11 @@ public class JelentkezesController {
 
             return "Már jelentkeztél erre a szakkörre!";
         }
-
+        Szakkor szakkor = szakkorRepository.findById(jelentkezes.getSzakkorId()).orElse(null);
+        long letszam = jelentkezesRepository.countBySzakkorId(jelentkezes.getSzakkorId());
+        if( letszam >= szakkor.getMaxLetszam()){
+            return "A szakkör betelt";
+        }
         jelentkezes.setJelentkezesDatum(
                 LocalDateTime.now().withNano(0)
         );
@@ -42,6 +65,17 @@ public class JelentkezesController {
     public List<Jelentkezes> getAllJelentkezes() {
         return jelentkezesRepository.findAll();
     }
+    @GetMapping("/user/{id}")
+    public List<Jelentkezes> getUserJelentkezesek(@PathVariable Long id){
 
+        return jelentkezesRepository.findByUserId(id);
+    }
+    @GetMapping("/szakkor/{id}")
+    public List<Jelentkezes> getSzakkorJelentkezoi(
+            @PathVariable Long id){
+
+        return jelentkezesRepository
+                .findBySzakkorId(id);
+    }
 
 }
--- src/main/java/hu/csongor/demo/repository/JelentkezesRepository.java
+++ src/main/java/hu/csongor/demo/repository/JelentkezesRepository.java
@@ -2,11 +2,14 @@ package hu.csongor.demo.repository;
 
 import hu.csongor.demo.entity.Jelentkezes;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import java.util.List;
 public interface JelentkezesRepository
         extends JpaRepository<Jelentkezes, Long> {
     boolean existsByUserIdAndSzakkorId(
             Long userId,
             Long szakkorId
     );
+    List<Jelentkezes> findByUserId(Long userId);
+    List<Jelentkezes> findBySzakkorId(Long szakkorId);
+    long countBySzakkorId(Long szakkorId);
 }
